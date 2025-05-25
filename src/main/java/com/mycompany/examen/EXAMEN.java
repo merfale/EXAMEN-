9e866048650fc7f395dbd585aef0947a47adc345
Fabian <0333232021@unjfsc.edu.pe>
2025-05-25 16:40:23 -0500

    iniciando

--- src/main/java/com/mycompany/examen/EXAMEN.java
+++ src/main/java/com/mycompany/examen/EXAMEN.java
@@ -11,6 +11,6 @@ package com.mycompany.examen;
 public class EXAMEN {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Jonathan");
     }
 }
