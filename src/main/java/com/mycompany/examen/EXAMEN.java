c98e8c5e7e3f299cb057a9807d94edfe14cd3fc5
Fabian <0333232021@unjfsc.edu.pe>
2025-05-25 16:38:17 -0500

    iniciando

---
+++ src/main/java/com/mycompany/examen/EXAMEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen;
+
+/**
+ *
+ * @author Merca
+ */
+public class EXAMEN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
