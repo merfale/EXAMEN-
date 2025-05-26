ffe180535440e7efc8f9de5ed490a707d9578d0c
USUARIO <USUARIO@DESKTOP-K0BP51I.bbrouter>
2025-05-26 13:06:00 -0500

    que sea lo que Dios quiera

--- src/main/java/com/mycompany/examen/EXAMEN.java
+++ src/main/java/com/mycompany/examen/EXAMEN.java
@@ -5,5 +5,6 @@ public class EXAMEN {
     public static void main(String[] args) {
         System.out.println("Hola Jonathan");
         System.out.println("Chinga zzzz");
+        System.out.println("Con fe");
     }
 }
